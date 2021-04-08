3c38847523d945b371c867afccf24379dc6eb3e3
Ondrej Vicar <ondra.vicar@gmail.com>
2021-04-08 17:16:55 +0200

    Fix comments

--- flaui-repro/Program.cs
+++ flaui-repro/Program.cs
@@ -17,12 +17,10 @@ process.OutputDataReceived += (obj, args) => Console.WriteLine(args.Data);
 process.Start();
 process.BeginOutputReadLine();
 
-System.Threading.Thread.Sleep(1000);
+System.Threading.Thread.Sleep(1000); // stdout being produced
 
 var application = Application.Attach(process);
-// this will dispoce process
-var window = application.GetMainWindow(new UIA3Automation());
+var window = application.GetMainWindow(new UIA3Automation()); // this will Dispose process, stdout of winforms will stop
 Console.WriteLine(window.Title);
 
-
-process.WaitForExit(); // will throw System.InvalidOperationException: No process is associated with this object
+process.WaitForExit(); // any interaction with process will throw System.InvalidOperationException: No process is associated with this object
